06013d4bf0f176952b0808282b1e525e2e1574a6
19Romit <108919000+19Romit@users.noreply.github.com>
2022-07-12 21:31:49 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,113 @@
+let questions = [
+  {
+  numb: 1,
+  question: "What does HTML stand for?",
+  answer: "Hyper Text Markup Language",
+  options: [
+      "Hyperlinks and Text Markup Language",
+      "Hyper Text Markup Language",
+      "Home Tool Markup Language",
+      "none of these"
+  ]
+},
+{
+  numb: 2,
+  question: "Which of the following JavaScript cannot do?",
+  answer: "All of the Above",
+  options: [
+    "JavaScript can react to events",
+    "JavaScript can manipulate HTML elements",
+    "JavaScript can be use to validate data",
+    "All of the Above"
+  ]
+},
+{
+  numb: 3,
+  question: "Which sign does jQuery use as a shortcut for jQuery?",
+  answer: "the $ sign",
+  options: [
+    "the # sign",
+    "the $ sign",
+    "the ? sign",
+    "None of these"
+  ]
+},
+{
+  numb: 4,
+  question: "Which jQuery method is used to hide selected elements?",
+  answer: "hide()",
+  options: [
+    "hidden()",
+    "visible(false)",
+    "hide()",
+    "display(none)"
+  ]
+},
+{
+  numb: 4,
+  question: "Which is not valid data type in Javascript?",
+  answer: "float",
+  options: [
+    "Boolean",
+    "float",
+    "Undefinded",
+    "Number"
+  ]
+},
+{
+  numb: 6,
+  question: "What does CSS stand for?",
+  answer: "Cascading Style Sheets",
+  options: [
+    "Creative Style Sheets",
+    "Colorful Style Sheets",
+    "Cascading Style Sheets",
+    "Computer Style Sheets"
+  ]
+},
+{
+  numb: 7,
+  question: "Which is the correct CSS syntax?",
+  answer: "body {color: black;}",
+  options: [
+    "body {color: black;}",
+    "{body:color=black;}",
+    "body:color=black;",
+    "{body;color:black;}"
+  ]
+},
+{
+  numb: 8,
+  question: "Which property is used to change the background color?",
+  answer: "background-color",
+  options: [
+    "background-color",
+    "bgcolor",
+    "color",
+    "All of these"
+  ]
+},
+{
+  numb: 9,
+  question: "How do you add a background color for all <h1> elements?",
+  answer: "h1 {background-color:#FFFFFF;}",
+  options: [
+    "all.h1 {background-color:#FFFFFF;}",
+    "h1.all {background-color:#FFFFFF;}",
+    "h1 {background-color:#FFFFFF;}",
+    "None of These"
+  ]
+},
+{
+  numb: 10,
+  question: "Which property is used to change the left margin of an element?",
+  answer: "margin-left",
+  options: [
+    "margin-left",
+    "padding-left",
+    "indent",
+    "None of these"
+  ]
+},
+
+];
